978b8b140ce5c5bebfe75b7b8283749c2ecf7bd8
Tim <corsairtim89@gmail.com>
2023-02-27 20:05:33 +0300

    setup refresh

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -1,12 +1,10 @@
 package org.example;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 @RequiredArgsConstructor
 public class MainController {
